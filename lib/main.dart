8811d21921b73b574a2ac9965be6dcac63889b6d
Willians Martins <professorwillians@gmail.com>
2020-06-09 00:14:10 -0300

    add lista com itens

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,31 @@ import 'package:flutter/material.dart';
 void main() =>
     runApp(MaterialApp(
       home: Scaffold(
+        body: Column(
+          children: <Widget>[
+            Card(
+              child: ListTile(
+                  leading: Icon(Icons.monetization_on),
+                  title: Text('100.0'),
+                  subtitle: Text('1000'),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('100.0'),
+                subtitle: Text('1000'),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('100.0'),
+                subtitle: Text('1000'),
+              ),
+            ),
+          ],
+        ),
           appBar: AppBar(
             title: const Text('Controle da Moto'),
           ),
